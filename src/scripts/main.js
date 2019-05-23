af1fa5f3f732b6946dce89ceb85f76cba8307c91
AlexHladun <gefest.gl@gmail.com>
2019-05-23 22:28:18 +0300

    destro function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
-
-console.log('hello mate academy!');
+function destroTarget() {
+  document.addEventListener('click', (evnt) => {
+    const target = document.elementFromPoint(evnt.clientX, evnt.clientY);
+    target.remove();
+  });
+};
+destroTarget();
